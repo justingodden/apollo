79c53a4d5e987ead20b0530e25b0cc76adc77c1b
justingodden <justingodden@gmail.com>
2021-12-19 19:52:17 +0000

    feat(modeller): add to main function to get from database and create feature embeddings

---
+++ modeller/main.py
@@ -0,0 +1,32 @@
+from sklearn.model_selection import train_test_split
+
+from data_extractor import get_data
+from embeddings_generator import Embedder
+
+
+def main() -> None:
+    embedder = Embedder()
+
+    df = get_data()
+    X, y = df.drop("price", axis=1), df["price"]
+    X["box"] = X["box"].map({True: 1, False: 0})
+    X["papers"] = X["papers"].map({True: 1, False: 0})
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+    embedder.create_embeddings(X,
+                               X_train,
+                               X_test,
+                               y_train,
+                               y_test)
+
+    X = X.join(embedder.generate_features(X["brand"],
+                                          X["series"])).drop(["brand", "series"], axis=1)
+    X_train = X_train.join(embedder.generate_features(X_train["brand"],
+                                                      X_train["series"])).drop(["brand", "series"], axis=1)
+    X_test = X_test.join(embedder.generate_features(X_test["brand"],
+                                                    X_test["series"])).drop(["brand", "series"], axis=1)
+
+
+
+if __name__ == "__main__":
+    main()
