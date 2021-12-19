4129f29d29c2f40e3fbef59b0f603d08d6812d89
justingodden <justingodden@gmail.com>
2021-12-19 20:43:41 +0000

    feat(modeller): add model creation to main file

--- modeller/main.py
+++ modeller/main.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 
 from data_extractor import get_data
 from embeddings_generator import Embedder
+import create_model
 
 
 def main() -> None:
@@ -21,11 +22,12 @@ def main() -> None:
 
     X = X.join(embedder.generate_features(X["brand"],
                                           X["series"])).drop(["brand", "series"], axis=1)
-    X_train = X_train.join(embedder.generate_features(X_train["brand"],
-                                                      X_train["series"])).drop(["brand", "series"], axis=1)
-    X_test = X_test.join(embedder.generate_features(X_test["brand"],
-                                                    X_test["series"])).drop(["brand", "series"], axis=1)
+    # X_train = X_train.join(embedder.generate_features(X_train["brand"],
+    #                                                   X_train["series"])).drop(["brand", "series"], axis=1)
+    # X_test = X_test.join(embedder.generate_features(X_test["brand"],
+    #                                                 X_test["series"])).drop(["brand", "series"], axis=1)
 
+    create_model.fit_model(X, y)
 
 
 if __name__ == "__main__":
