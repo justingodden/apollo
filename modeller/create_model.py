975be38f5945b3dc8a52e6fdb00f7c702f5320ac
justingodden <justingodden@gmail.com>
2021-12-19 20:39:37 +0000

    feat(modeller): add LightGBM and Optuna autoCV model tuner

---
+++ modeller/create_model.py
@@ -0,0 +1,39 @@
+import os
+
+import pandas as pd
+from sklearn.model_selection import KFold
+from lightgbm import early_stopping
+from lightgbm import log_evaluation
+import optuna.integration.lightgbm as lgb_tuner
+
+
+def fit_model(X_train: pd.DataFrame, y_train: pd.Series):
+    dtrain = lgb_tuner.Dataset(X_train, label=y_train)
+
+    params = {
+        "objective": "regression",
+        "metric": "mean_squared_error",
+        "boosting_type": "gbdt",
+    }
+
+    tuner = lgb_tuner.LightGBMTunerCV(params,
+                                      dtrain,
+                                      num_boost_round=3000,
+                                      folds=KFold(n_splits=5),
+                                      callbacks=[early_stopping(100),
+                                                 log_evaluation(100)
+                                                 ],
+                                      return_cvbooster=True
+                                      )
+
+    tuner.run()
+
+    model = tuner.get_best_booster()
+
+    output_path = os.path.join(os.getcwd(), "output")
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    model.save_model(os.path.join(output_path, "lgb_model.txt"))
+    best_score_rmse = tuner.best_score**0.5
+    # best_params = tuner.best_params
+    # print(best_score_rmse)
